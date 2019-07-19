959ba27d8afb0f4279d0c5f75ed89f569ff3170e
Julian Rivera <superjuarri11@gmail.com>
2019-07-18 17:43:22 -0700

    Updated styled-components options

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-styled-components`,
       options: {
-        // Add any options here
+        displayName: false,
       },
     },
     `gatsby-plugin-react-helmet`,
