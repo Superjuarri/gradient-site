e9321db98637a418f234a8038eb8f628728a9867
Julian Rivera <superjuarri11@gmail.com>
2019-07-31 16:51:16 -0700

    Button styled updated

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,26 +1,63 @@
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import { animated, useSpring } from 'react-spring'
 
-const Button = styled.button`
-  z-index: 1;
-  position: relative;
-  font-size: inherit;
-  font-weight: 500;
-  font-family: inherit;
+const Wrapper = styled(animated.button)`
+  min-width: 125px;
 
-  color: #fff;
-  background: var(--main-color);
-  padding: 0.6rem 0.8rem;
-  outline: none;
+  padding: 1rem 1.4rem;
+  background: var(--bright_pink);
+
+  font-size: 0.7rem;
+  font-weight: 700;
+  color: var(--off_white);
+  letter-spacing: 0.125rem;
+  text-transform: uppercase;
+  appearance: none;
   border: none;
-  border-radius: 0.2rem;
+  border-radius: 0.6rem;
+  box-shadow: 0 10px 20px -10px var(--dull_pink);
+  outline: none;
+  position: relative;
+  z-index: 2;
+  cursor: pointer;
 
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
+  &:hover {
+    background: lighten($bright_pink, 2%);
+    box-shadow: 0 8px 65px -5px $dull_pink;
+  }
 
-  :hover {
-    cursor: pointer;
+  &:active {
+    background: $dull_pink;
+    box-shadow: 0 10px 60px -10px $dull_pink;
+    transform: perspective(250px) scale3d(1, 1, 1) translateY(5%);
   }
 `
+
+const Button = ({ children, handleChange }) => {
+  const [hover, setHover] = useState(false)
+
+  const { s } = useSpring({
+    s: hover ? 1.06 : 1,
+    config: {
+      mass: 1,
+      tension: 1000,
+      friction: 50,
+    },
+  })
+
+  return (
+    <Wrapper
+      onClick={() => handleChange}
+      onFocus={() => setHover(!hover)}
+      onBlur={() => setHover(!hover)}
+      onMouseEnter={() => setHover(!hover)}
+      onMouseLeave={() => setHover(!hover)}
+      style={{ transform: s.interpolate(s => `scale(${s})`) }}
+    >
+      {children}
+    </Wrapper>
+  )
+}
 
 export default Button
